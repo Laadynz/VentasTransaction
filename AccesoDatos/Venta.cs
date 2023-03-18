b4beb500e8c0eee409e04e9fc80a2a78b556cdc8
LadyNunez <laadynz@hotmail.com>
2023-03-18 14:24:59 -0700

    Hasta aqui

--- AccesoDatos/Venta.cs
+++ AccesoDatos/Venta.cs
@@ -6,13 +6,13 @@ using System.Threading.Tasks;
 
 namespace AccesoDatos
 {
-   public class Venta
+    public class Venta
     {
         public int Id { get; set; }
         public int Folio { get; set; }
         public DateTime Fecha { get; set; }
         public int CLienteId { get; set; }
         public decimal Total { get; set; }
-        public List<VentaDetalle> Conceptos { get; set; }
+        public List<VentaDetalle> Conceptos { get; set; } = new List<VentaDetalle>();
     }
 }
